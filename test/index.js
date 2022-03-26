fc6f98a800849055098baa84f403ceec52152603
wlllyfor <958167224@qq.com>
2022-03-27 07:41:43 +0800

    feat：test

--- test/index.js
+++ test/index.js
@@ -1,25 +1,30 @@
-/**
- * 思路，遍历数组，选出最小值
- * 把最小值挑出来，push 进新数组，原数组删除这个最小值
- * 只要原数组有值，就重复上面的过程，原数组删完时，也就选完了
- * 返回新数组
- */
+const cache = {}
 
-function selectSort(arr) {
-  const res = []
-  while(arr.length) {
-    let minIndex = 0
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < arr[minIndex]) {
-        minIndex = i
-      }
-    }
-    res.push(arr[minIndex])
-    arr.splice(minIndex, 1)
+function isPrime (n) {
+  if (n in cache) {
+    console.log(`cache中有${n}，直接返回 :>> `, cache[n])
+    return cache[n]
   }
+
+  let res = n > 1
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      res = false
+      break
+    }
+  }
+
+  console.log(`cache中没有${n}，存进去 :>> 是否是素数`, res)
+  cache[n] = res
+
   return res
 }
 
-const arr = [7,2,6,8,1,5,13,12,4,9,17]
+isPrime(3)
+isPrime(4)
+isPrime(5)
+isPrime(5)
+isPrime(5)
 
-console.log(selectSort(arr))
+console.log('cache :>> ', cache)
