d14fcf25a127280fbe0fb79e5b40544deaa68a4f
wlllyfor <958167224@qq.com>
2022-03-30 09:10:51 +0800

    feat：update blog

--- test/index.js
+++ test/index.js
@@ -1,47 +1,15 @@
-function exist (board, word) {
-
-  if (board.length === 0 ) {
-    return false
-  }
-
-  if (word.length === 0) {
-    return true
-  }
-
-  let row = board.length
-  let col = board[0].length
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < col; j++) {
-      const res = find(i, j, 0)
-      if (res) {
-        return true
-      }
-    }
-  }
-  return false
-
-  function find (i, j, curIndex) {
-    if (i >= row || i < 0 || j >= col || j < 0) {
-      return false
-    }
-  
-    let letter = board[i][j]
-  
-    if (letter !== word[curIndex]) {
-      return false
-    }
-  
-    if (curIndex === word.length - 1) {
-      return true
-    }
-  
-    board[i][j] = null // 标记，已经找过的字母置为 null
-    const res = find(i + 1, j, curIndex + 1)
-             || find(i - 1, j, curIndex + 1)
-             || find(i, j + 1, curIndex + 1)
-             || find(i, j - 1, curIndex + 1)
-    board[i][j] = letter
-  
-    return res
-  }
+let a = 100
+const foo = () => () => {
+  console.log(this.a)
 }
+
+const obj1 = {
+  a: 2
+}
+
+const obj2 = {
+  a: 3
+}
+
+const bar = foo.call(obj1)
+bar.call(obj2)
