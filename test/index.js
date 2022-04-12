1516edb64580e06c739fe57940feff3d7650ecef
wlllyfor <958167224@qq.com>
2022-04-12 09:21:08 +0800

    feat：update hasDone

--- test/index.js
+++ test/index.js
@@ -1,2 +1,25 @@
-const ele = document.getElementById('element')
-console.log(ele)
+const node = {
+  tag: 'div',
+  children: [
+    {
+      tag: 'h1',
+      children: 'hello'
+    }
+  ]
+}
+
+function render (node, root) {
+  const el = document.createElement(node.tag)
+  if (typeof node.children === 'string') {
+    const text = document.createTextNode(node.children)
+    el.appendChild(text)
+  }
+  if (Array.isArray(node.children)) {
+    node.children.forEach(child => render(child, el))
+  }
+  root.appendChild(el)
+}
+
+const app = document.getElementById('app')
+
+render(node, app)
