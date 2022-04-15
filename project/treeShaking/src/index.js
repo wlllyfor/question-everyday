9117d7003f0735bb5ef556feabd234be447bd6bb
wlllyfor <958167224@qq.com>
2022-04-15 16:35:06 +0800

    feat: add tree-shaking

---
+++ project/treeShaking/src/index.js
@@ -0,0 +1,4 @@
+import { foo } from './a'
+
+foo()
+
---
+++ test/index.js
@@ -0,0 +1,9 @@
+const o = new Map()
+
+o.set('a', 'a')
+o.set('b', 'b')
+o.set('c', 'c')
+
+console.log('o :>> ', o);
+console.log('o.keys :>> ', o.keys());
+console.log('o.keys().next :>> ', o.keys().next());
