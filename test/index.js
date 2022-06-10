f300a43cfd7899acb8a20b2d5dd1e9878e407077
wlllyfor <958167224@qq.com>
2022-06-10 20:12:06 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,60 +1,19 @@
-// function myInstanceof(target, Fn) {
-  
-//   if(!['object', 'function'].includes(typeof target) || !target) {
-//     return false
-//   }
-//   while(true) {
-//     if (!target) {
-//       return false
-//     } else if (target === Fn.prototype) {
-//       return true
-//     }
-//     target = target.__proto__
-//   }
-// }
+function myGet(object, path, defaultValue) {
 
-function myInstanceof(target, Fn) {
-  if(!['object', 'function'].includes(typeof target) || !target) {
-    return false
+  let newPath
+  if (Array.isArray(path)) {
+    newPath = path
+  } else {
+    newPath = path.replace(/\[(\d+)\]/g, '.$1').split('.')
   }
-  if (!target) {
-    return false
-  } else if (target === Fn.prototype) {
-    return true
+
+  let res = object
+  for (const p of newPath) {
+    res = (res || {})[p]
   }
-  return myInstanceof(target.__proto__, Fn)
+  return res || defaultValue
 }
 
-// console.log(myInstanceof(1, Object))
-// console.log(myInstanceof(new Number(1), Object))
-// console.log(myInstanceof(Number(1), Number))
-// console.log(myInstanceof(new Number(1), Number))
-// console.log(1 instanceof Object)
-// console.log(new Number(1) instanceof Number)
-// console.log(new Number(1) instanceof Number)
+const obj = {}
 
-console.log(myInstanceof(undefined, Object))
-
-console.log(undefined instanceof Object)
-
-
-// console.log(myInstanceof('qwe', Object))
-// console.log(myInstanceof('qwe', String))
-// console.log(myInstanceof(new String('qwe'), String))
-
-
-// console.log(myInstanceof([], Array))
-// console.log(myInstanceof([], Object))
-// console.log([] instanceof Object)
-// console.log(myInstanceof({}, Object))
-
-// console.log(myInstanceof(function fn() {}, Function))
-// console.log(myInstanceof(function fn() {}, Object))
-// let fn = function () {}
-// console.log(fn instanceof Function)
-// console.log(fn instanceof Object)
-
-// console.log(myInstanceof(new Date(), Date))
-// console.log(myInstanceof(new Date(), Object))
-// console.log(myInstanceof(/abc/, RegExp))
-// console.log(myInstanceof(/abc/, Object))
+console.log(_.get(obj, 'a.b')) // 输出 undefined
