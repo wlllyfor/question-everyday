ef06337756e22255be3c57e23b5dbea7ef4a2be1
王林 <958167224@qq.com>
2022-07-15 17:11:21 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,18 +1,23 @@
-function foo() {
-  return () => {
-    console.log(this.a)
+function myBind(fn, context) {
+  const args = [...arguments].slice(2)
+
+  // 给返回的函数增加一个名称 newFn，方便后面使用
+  return function newFn () {
+
+    // 如果
+    console.log('this :>> ', this);
+    if (this instanceof newFn) { 
+      return new fn(...args, ...arguments) 
+    }
+    return fn.call(context, ...args, ...arguments)
   }
 }
 
-const obj1 = {
-  a: 2
+function Person (name) {
+  this.name = name
 }
 
-const obj2 = {
-  a: 3
-}
- 
-const bar = foo.call(obj1) // ?
-console.log('bar :>> ', bar);
-bar()
-bar.call(obj2)             // ? 
+const tmpFn = myBind(Person)
+const p = tmpFn('zhangsan')
+console.log(p) // 输出 {}，应该输出实例才对
+console.log(p.name) // 输出 undefined，应该输出 zhangsan 才对
