7cc7d63f7c3c2cd9c0a16898fbbd11f179a23db6
王林 <958167224@qq.com>
2022-07-18 16:58:09 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,23 +1,9 @@
-function myBind(fn, context) {
-  const args = [...arguments].slice(2)
-
-  // 给返回的函数增加一个名称 newFn，方便后面使用
-  return function newFn () {
-
-    // 如果
-    console.log('this :>> ', this);
-    if (this instanceof newFn) { 
-      return new fn(...args, ...arguments) 
-    }
-    return fn.call(context, ...args, ...arguments)
+// 用立即执行函数
+const fn = (function () { // 直接写匿名函数，不用起函数名
+  const name = 'zhangsan'
+  return function () {
+    console.log(name)
   }
-}
+})()
 
-function Person (name) {
-  this.name = name
-}
-
-const tmpFn = myBind(Person)
-const p = tmpFn('zhangsan')
-console.log(p) // 输出 {}，应该输出实例才对
-console.log(p.name) // 输出 undefined，应该输出 zhangsan 才对
+fn() // 输出 'zhangsan'
