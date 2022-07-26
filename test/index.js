8a10744b9c850c197f8952c0d13f33e9c03e206e
王林 <958167224@qq.com>
2022-07-26 13:43:24 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,13 +1,36 @@
+// function myInterval (fn, delay) {
+//   let timer = null
+//   function helper() {
+//     fn()
+//     timer = setTimeout(() => {
+//       helper()
+//     }, delay)
+//   }
+//   setTimeout(helper, delay)
+//   return () => clearTimeout(timer)
+// }
 
+// setInterval(function () {
+//   console.log('执行 fn')
+//   if (a.b) {
+//     // do something
+//   }
+// }, 1000)
 
-function myInterval(fn, delay, ...args) {
-  let timer = null;
-  function helper(){
-    fn.apply(this, args);
-    timer = setTimeout(helper, delay) // 递归调用
-  }
-  setTimeout(helper, delay)
-}
+// let start = Date.now()
+// setInterval(() => {
+//   const now = Date.now()
+//   console.log('interval', now - start)
+//   start = now
+// }, 1000);
+
+// myInterval(function () {
+//   console.log('执行 fn')
+//   if (a.b) {
+//     // do something
+//   }
+// }, 1000)
+
 
 // myInterval(() => {
 //   console.log('1 :>> ', 1);
@@ -42,34 +65,65 @@ function myInterval(fn, delay, ...args) {
 //   start = now
 // }, 1000);
 
-let start = Date.now()
-myInterval(() => {
-  const now = Date.now()
-  linearSearch(createNums(100000000), 100000000)
-  console.log('interval', now - start)
-  start = now
-}, 1000);
+// let start = Date.now()
+// myInterval(() => {
+//   const now = Date.now()
+//   linearSearch(createNums(100000000), 100000000)
+//   console.log('interval', now - start)
+//   start = now
+// }, 1000);
 
 
-// 线性查找列表 nums 中是否存在 target，存在返回 true，不存在返回 false
-function linearSearch(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) {
-      return true;
-    }
-  }
-  return false;
-}
+// // 线性查找列表 nums 中是否存在 target，存在返回 true，不存在返回 false
+// function linearSearch(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === target) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
-// 创建长度为 n 的数组
-function createNums(n) {
-  let res = [];
-  for (let i = 1; i <= n; i++) {
-    res[i - 1] = i;
-  }
-  return res;
-}
+// // 创建长度为 n 的数组
+// function createNums(n) {
+//   let res = [];
+//   for (let i = 1; i <= n; i++) {
+//     res[i - 1] = i;
+//   }
+//   return res;
+// }
 
 // console.time('线性查找')
 // linearSearch(createNums(100000000), 100000000)
 // console.timeEnd('线性查找')
+
+
+// 倒计时间隔为 1000 毫秒
+const gap = 1000
+// 活动开始倒计时初始值，假设为 1 分钟
+let totalTime = 60000  
+// 辅助变量，每次递归调用 + 1，用来计算时间偏差
+let count = 0
+const start = Date.now()
+let timer
+if( totalTime >= 0) {
+  timer = setTimeout(interval, gap)
+}
+ 
+function interval () {
+  count++
+  // 计算出当前执行倒计时的时间与实际应执行时间的偏差
+  const offset = Date.now() - (start + count * gap) 
+  let calcTime = gap - offset
+  if (calcTime < 0) { 
+    calcTime = 0 
+  }
+  totalTime -= gap
+  console.log(`误差：${offset} ms，下一次执行：${calcTime} 毫秒后，离活动开始还有：${totalTime} 毫秒`)
+  if (totalTime < 0) {
+    clearTimeout(timer)
+  } else {
+    // 下一次执行的时间，就用计算后校准的时间 calcTime
+    timer = setTimeout(interval, calcTime)
+  }
+}
