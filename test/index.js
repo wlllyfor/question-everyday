4c7f9211566404639727f1da04c0d6c3218982ca
王林 <958167224@qq.com>
2022-07-29 11:25:07 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,129 +1,59 @@
-// function myInterval (fn, delay) {
-//   let timer = null
-//   function helper() {
-//     fn()
-//     timer = setTimeout(() => {
-//       helper()
-//     }, delay)
-//   }
-//   setTimeout(helper, delay)
-//   return () => clearTimeout(timer)
-// }
-
-// setInterval(function () {
-//   console.log('执行 fn')
-//   if (a.b) {
-//     // do something
-//   }
-// }, 1000)
-
-// let start = Date.now()
-// setInterval(() => {
-//   const now = Date.now()
-//   console.log('interval', now - start)
-//   start = now
-// }, 1000);
-
-// myInterval(function () {
-//   console.log('执行 fn')
-//   if (a.b) {
-//     // do something
-//   }
-// }, 1000)
-
-
-// myInterval(() => {
-//   console.log('1 :>> ', 1);
-// }, 1000)
-
-// setInterval(function () {
-//   console.log('执行 fn')
-//   if (a.b) {
-//     // do something
-//   }
-// }, 1000)
-
-// const startDate = new Date();
-// let endData;
-// // 第一个调用会被略过
-// setInterval(() => {
-//   console.log('start');
-//   console.log(startDate.getTime());
-//   console.log(endDate.getTime());
-//   console.log('end');
-// }, 1000);
-// while (startDate.getTime() + 2 * 1000 > (new Date()).getTime()) {
-// }
-// endDate = new Date();
-
-
-// let start = Date.now()
-// setInterval(() => {
-//   const now = Date.now()
-//   linearSearch(createNums(10000000), 10000000)
-//   console.log('interval', now - start)
-//   start = now
-// }, 1000);
-
-// let start = Date.now()
-// myInterval(() => {
-//   const now = Date.now()
-//   linearSearch(createNums(100000000), 100000000)
-//   console.log('interval', now - start)
-//   start = now
-// }, 1000);
-
-
-// // 线性查找列表 nums 中是否存在 target，存在返回 true，不存在返回 false
-// function linearSearch(nums, target) {
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] === target) {
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-
-// // 创建长度为 n 的数组
-// function createNums(n) {
-//   let res = [];
-//   for (let i = 1; i <= n; i++) {
-//     res[i - 1] = i;
-//   }
-//   return res;
-// }
-
-// console.time('线性查找')
-// linearSearch(createNums(100000000), 100000000)
-// console.timeEnd('线性查找')
-
-
-// 倒计时间隔为 1000 毫秒
-const gap = 1000
-// 活动开始倒计时初始值，假设为 1 分钟
-let totalTime = 60000  
-// 辅助变量，每次递归调用 + 1，用来计算时间偏差
-let count = 0
-const start = Date.now()
-let timer
-if( totalTime >= 0) {
-  timer = setTimeout(interval, gap)
+const time = (timer) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, timer)
+  })
 }
- 
-function interval () {
-  count++
-  // 计算出当前执行倒计时的时间与实际应执行时间的偏差
-  const offset = Date.now() - (start + count * gap) 
-  let calcTime = gap - offset
-  if (calcTime < 0) { 
-    calcTime = 0 
-  }
-  totalTime -= gap
-  console.log(`误差：${offset} ms，下一次执行：${calcTime} 毫秒后，离活动开始还有：${totalTime} 毫秒`)
-  if (totalTime < 0) {
-    clearTimeout(timer)
-  } else {
-    // 下一次执行的时间，就用计算后校准的时间 calcTime
-    timer = setTimeout(interval, calcTime)
-  }
+
+// const fetch1 = () => time(2000).then((resolve) => {
+//   console.log(1);
+//   return 1
+// })
+// const fetch2 = () => time(1000).then(() => {
+//   console.log(2);
+//   return 2
+// })
+// const fetch3 = () => time(1000).then(() => {
+//   console.log(3);
+//   return 3
+// })
+
+const fetch1 = () => {
+  return new Promise((resolve) => {
+    resolve(1)
+  })
+}
+
+const fetch2 = () => {
+  return new Promise((resolve) => {
+    resolve(2)
+  })
+}
+
+const fetch3 = () => {
+  return new Promise((resolve) => {
+    resolve(3)
+  })
+}
+
+mergePromise([fetch1, fetch2, fetch3]).then(data => {
+  console.log("done");
+  console.log(data); // data 为 [1, 2, 3]
+})
+
+function mergePromise (fnArray) {
+  // 存放每个ajax的结果
+  const data = [];
+  let p = Promise.resolve();
+  fnArray.forEach(fn => {
+  	// 第一次的then为了用来调用ajax
+  	// 第二次的then是为了获取ajax的结果
+    p = p.then(fn).then(res => {
+      data.push(res);
+      return data; // 把每次的结果返回
+    })
+  })
+  // 最后得到的promise它的值就是data
+  return p;
 }
