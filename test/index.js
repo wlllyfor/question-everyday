037cefa58e4f29f679d48e00813dc4e25511d28e
王林 <958167224@qq.com>
2022-09-05 09:36:45 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,62 +1,17 @@
-// function fn() {
-//   return myAsync(function* () {
-//     return 1
-//   });
-// }
-
-// function myAsync(genFn) {
-//   return new Promise(function(resolve, reject) {
-//     const gen = genFn();
-//     function step(nextFn) {
-//       let next;
-//       try {
-//         next = nextFn();
-//       } catch(e) {
-//         return reject(e);
-//       }
-//       if(next.done) {
-//         return resolve(next.value);
-//       }
-//       Promise.resolve(next.value).then(function(v) {
-//         step(function() { return gen.next(v); });
-//       }, function(e) {
-//         step(function() { return gen.throw(e); });
-//       });
-//     }
-//     step(function() { return gen.next(); });
-//   });
-// }
-
-// const p = fn()
-// p.then((val) => {
-//   console.log(val)
-// })
-
-// const fs = require('fs');
-
-// const readFile = function (fileName) {
-//   return new Promise(function (resolve, reject) {
-//     fs.readFile(fileName, function(error, data) {
-//       if (error) return reject(error);
-//       resolve(data);
-//     });
-//   });
-// };
-
-// const gen = function* () {
-//   const f1 = yield readFile('/xxx/a');
-//   const f2 = yield readFile('/xxx/b');
-//   console.log(f1.toString());
-//   console.log(f2.toString());
-// };
-
-function * printNum() {
-  yield 1
-  yield 2
-  return 3
+function swapPairs (node1) {
+  if (!node1 || !node1.next) {
+    return node1
+  }
+  const node2 = node1.next
+  node1.next = swapPairs(node2.next)
+  node2.next = node1
+  return node2
 }
 
-fn = printNum()
-console.log(fn.next()) // {value: 1, done: false}
-console.log(fn.next()) // {value: 2, done: false}
-console.log(fn.next()) // {value: 3, done: true}
+
+/**
+ *           
+ * node1 -> node2 -> ...
+ *                
+ * 
+ */
